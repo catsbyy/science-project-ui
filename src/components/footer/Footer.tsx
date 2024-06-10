90c24ad160bd28a303ca51e4cf0762f388b242fb
v-andrusenko <andrusenko.vp1@gmail.com>
2024-06-10 17:51:47 +0300

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,49 @@
 import "./Footer.css";
+import { LogoFacebook } from "react-ionicons";
+import { LogoTwitter } from "react-ionicons";
+import { LogoLinkedin } from "react-ionicons";
+import { LogoInstagram } from "react-ionicons";
+import { LogoYoutube } from "react-ionicons";
 
 const Footer = () => {
-    return ( 
-        <footer>
-            <p>
-            © Science Project, 2024
-            </p>
-        </footer>
-     );
-}
- 
+
+  return (
+    <footer className="footer">
+      <ul className="social-media">
+        <li className="social-icon">
+          <a className="social-icon-link" href="#">
+            <LogoFacebook></LogoFacebook>
+          </a>
+        </li>
+
+        <li className="social-icon">
+          <a className="social-icon-link" href="#">
+            <LogoTwitter></LogoTwitter>
+          </a>
+        </li>
+
+        <li className="social-icon">
+          <a className="social-icon-link" href="#">
+            <LogoLinkedin></LogoLinkedin>
+          </a>
+        </li>
+
+        <li className="social-icon">
+          <a className="social-icon-link" href="#">
+            <LogoInstagram></LogoInstagram>
+          </a>
+        </li>
+
+        <li className="social-icon">
+          <a className="social-icon-link" href="#">
+            <LogoYoutube></LogoYoutube>
+          </a>
+        </li>
+      </ul>
+
+      <p>@ 2024 TechWave | All Rights Reserved</p>
+    </footer>
+  );
+};
+
 export default Footer;
